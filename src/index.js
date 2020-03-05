9a8e11e29dda06f252ceeb3f3661e92a870d31fb
Yongyi Xu <reach.yongyixu@gmail.com>
2020-03-05 19:06:30 +0700

    fix port issue

--- src/index.js
+++ src/index.js
@@ -25,6 +25,6 @@ const server = new ApolloServer({ typeDefs, resolvers, context });
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
+app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
